bb69c39eb8280dbe1be6e6fbc335d7a361a0e279
GOMEZ GIANKARLO <87879526+Gianka200414@users.noreply.github.com>
2025-11-08 02:15:14 +0000

    IntArray header file complete

--- HW4/IntArray.h
+++ HW4/IntArray.h
@@ -1,12 +1,33 @@
-// IntArray.h G.G
+// IntArray.h  G.G
 #ifndef INTARRAY_H
 #define INTARRAY_H
 
+#include <iostream>
+using namespace std;
+
 class IntArray
 {
+private:
+    int* arr;                  // pointer to a dynamic integer array G.G
+    static int total_entries;  // total number of array entries across all instances G.G
+    int size;                  // number of values in this instance G.G
+
 public:
-    IntArray();  // empty constructor for now G.G
-    ~IntArray(); // empty destructor for now G.G
+    // Constructors and Destructor G.G
+    IntArray();          // default constructor (size = 5) G.G
+    IntArray(int size);  // parameterized constructor G.G
+    ~IntArray();         // destructor (release dynamic memory) G.G
+
+    // Member Functions
+    int getSize() const;                // return number of values in this instance G.G
+    int getValue(int index) const;      // return value at index (assert index is legal) G.G
+    void replace(int index, int value); // replace value at index (assert index is legal) G.G
+    void swap(int index_1, int index_2);// swap two values (assert both indexes are legal) G.G
+
+    void print() const;                 // print all array values on one line G.G
+
+    static int get_total_entries();     // return total number of entries across all instances G.G
+    float get_instance_percent() const; // (size / total_entries) * 100 G.G
 };
 
 #endif
